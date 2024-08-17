892606ae0d9a7abed39653906227cf6b557c9a36
viraj-10 <virajjoshi132@gmail.com>
2024-08-17 23:41:52 +0530

    feat: added all the components in the gluestack

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,12 @@
+import { Button, ButtonText } from "@/components/ui/button";
+import { Center } from "@/components/ui/center";
 import { View } from "react-native";
 export default function HomeScreen() {
-  return <View className="flex-1 bg-primary-500" />;
+  return (
+    <Center className="flex-1 ">
+      <Button>
+        <ButtonText>Home</ButtonText>
+      </Button>
+    </Center>
+  );
 }
